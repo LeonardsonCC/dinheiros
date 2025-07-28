7fb89314c342a206043f85708b44fa7762ee3d14
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-07-28 19:48:57 -0300

    Making TypeScript any type a warning instead of error

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -13,7 +13,8 @@ export default [
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
-      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn"
     },
     settings: {
       react: {
