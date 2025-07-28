e74f66aeb3ad842f906e956a9e50add70fd435bc
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-07-28 19:06:13 -0300

    Updating App.tsx to show landing page for unauthenticated users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Transactions from './pages/Transactions';
 import AllTransactions from './pages/AllTransactions';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import LandingPage from './pages/LandingPage';
 import NewAccount from './pages/NewAccount';
 import EditAccount from './pages/EditAccount';
 import NewTransaction from './pages/NewTransaction';
@@ -26,12 +27,12 @@ function App() {
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <Toaster position="top-right" />
       <Routes>
-        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/" />} />
-        <Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to="/" />} />
+        <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/dashboard" />} />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />} />
+        <Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to="/dashboard" />} />
         <Route path="/accept-invitation" element={isAuthenticated ? <AcceptInvitation /> : <Navigate to="/login" />} />
-        <Route path="/" element={isAuthenticated ? <Layout /> : <Navigate to="/login" />}>
+        <Route path="/dashboard" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
           <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
           <Route path="accounts">
             <Route index element={<Accounts />} />
             <Route path="new" element={<NewAccount />} />
@@ -53,6 +54,40 @@ function App() {
           <Route path="statistics" element={<Statistics />} />
           <Route path="categories" element={<CategoryManager />} />
           <Route path="categorization-rules" element={<CategorizationRules />} />
+        </Route>
+        
+        {/* Authenticated routes that need Layout wrapper */}
+        <Route path="/accounts" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<Accounts />} />
+          <Route path="new" element={<NewAccount />} />
+          <Route path=":accountId/edit" element={<EditAccount />} />
+          <Route path=":accountId/transactions" element={<Transactions />} />
+          <Route path=":accountId/transactions/new" element={<NewTransaction />} />
+          <Route path=":accountId/transactions/import" element={<ImportTransactions />} />
+          <Route path="transactions/import" element={<ImportTransactions />} />
+          <Route path=":accountId/transactions/:transactionId/edit" element={<EditTransaction />} />
+          <Route path="transactions/new" element={<NewTransaction />} />
+          <Route path="transactions" element={<AllTransactions />} />
+        </Route>
+        
+        <Route path="/shared-accounts" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<SharedAccounts />} />
+        </Route>
+        
+        <Route path="/profile" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<Profile />} />
+        </Route>
+        
+        <Route path="/statistics" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<Statistics />} />
+        </Route>
+        
+        <Route path="/categories" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<CategoryManager />} />
+        </Route>
+        
+        <Route path="/categorization-rules" element={isAuthenticated ? <Layout /> : <Navigate to="/" />}>
+          <Route index element={<CategorizationRules />} />
         </Route>
       </Routes>
     </div>
